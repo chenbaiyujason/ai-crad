10c79a995a62485a13444708f24ff81094cea3b2
arrayJY <i@jiyuan.one>
2024-06-17 01:13:16 +0800

    拍摄+上传

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,24 @@
 "use client";
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import localFont from "next/font/local";
 import "./page.css";
 const myFont = localFont({ src: "./fusion-pixel.ttf" });
-import { ImageUpload } from "@/components/ui/ImageUpload";
+
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const snapshotImgRef = useRef<HTMLImageElement>(null);
+  const uploadImgRef = useRef<HTMLImageElement>(null);
+  const uploadImgInputRef = useRef<HTMLInputElement>(null);
   const [videoAspectRatio, setVideoAspectRatio] = useState(1);
   const [snapshotImg, setSnapshotImg] = useState("");
+  const [uploadImg, setUploadImg] = useState("");
   const [resultText, setResultText] = useState("");
   const [resultImg, setResultImg] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [isShowSnapshot, setIsShowSnapshot] = useState(false);
   const [isResultShow, setIsResultShow] = useState(false);
-  const [referImage, setReferImage] = useState<string | undefined>(undefined)
-  const [referImageMode, setReferImageMode] = useState("")
-  const onUploadReferImage = (value: string) => {
-    setReferImage(value)
-  }
+
   useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({
@@ -54,20 +52,25 @@ export default function Home() {
   };
 
   const snapshot = () => {
-    if (!videoRef?.current || !canvasRef?.current || !snapshotImgRef?.current) {
+    if (!videoRef?.current) {
       return;
     }
-    const canvas = canvasRef.current;
     const video = videoRef.current;
-    const ctx = canvas.getContext("2d");
-    const ratio = window.devicePixelRatio || 1;
-    ctx?.scale(ratio, ratio);
-
     let videoRatio =
       videoAspectRatio < 1 ? 1 / videoAspectRatio : videoAspectRatio;
     let finalWidth = video.offsetWidth;
     let finalHeight = finalWidth * videoRatio;
+    drawOnCanvas(video, finalWidth, finalHeight);
+  };
 
+  const drawOnCanvas = (source: any, width: number, height: number) => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return;
+    }
+    const ratio = window.devicePixelRatio || 1;
+    let finalWidth = width * ratio,
+      finalHeight = height * ratio;
     if (finalWidth > finalHeight) {
       const scale = 1024 / finalWidth;
       finalWidth = 1024;
@@ -77,11 +80,10 @@ export default function Home() {
       finalHeight = 1024;
       finalWidth *= scale;
     }
-
-    canvas.width = finalWidth * ratio;
-    canvas.height = finalHeight * ratio;
-
-    ctx?.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const ctx = canvas.getContext("2d");
+    canvas.width = finalWidth;
+    canvas.height = finalHeight;
+    ctx?.drawImage(source, 0, 0, canvas.width, canvas.height);
     setSnapshotImg(canvas.toDataURL("image/jpg"));
     setIsShowSnapshot(true);
   };
@@ -97,7 +99,7 @@ export default function Home() {
     }, 3000);
     */
 
-    const url = "/create"; //TODO
+    const url = "/api/create"; //TODO
 
     const data = {
       base_64: snapshotImg,
@@ -143,6 +145,29 @@ export default function Home() {
     setIsShowSnapshot(false);
   };
 
+  const onUploadChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) {
+      return;
+    }
+    const img = uploadImgRef.current;
+    if (!img) {
+      return;
+    }
+    const file = event.target.files[0];
+    const fr = new FileReader();
+    fr.onloadend = () => {
+      if (fr.result) {
+        setUploadImg(fr.result as string);
+        setIsLoading(true)
+        img.onload = () => {
+          drawOnCanvas(img, img.width, img.height);
+          img.onload = null;
+        };
+      }
+    };
+    fr.readAsDataURL(file);
+  };
+
   return (
     <main className={"h-dvh " + myFont.className}>
       <div className="flex h-full w-full flex-col items-center justify-center p-0 absolute">
@@ -163,17 +188,14 @@ export default function Home() {
           }
         >
           <img
-            ref={snapshotImgRef}
+            ref={uploadImgRef}
             src={snapshotImg}
             className="h-full main-img"
           />
         </div>
-        <div className="flex items-center jusify-center absolute bottom-px z-10 pb-10">
-          {isResultShow ? null : (
-            <button
-              className="btn btn-blue inline-flex"
-              onClick={() => submit()}
-            >
+        <div className="flex absolute bottom-px z-10 pb-10 w-full justify-center">
+          <div className="px-3">
+            <label className="btn btn-blue inline-flex" htmlFor="capture-img">
               {isLoading ? (
                 <img
                   src="/loading.svg"
@@ -181,21 +203,54 @@ export default function Home() {
                 ></img>
               ) : null}
               拍摄
-            </button>
-          )}
+            </label>
+            <input
+              ref={uploadImgInputRef}
+              id="capture-img"
+              type="file"
+              accept="image/*"
+              className="file-input"
+              multiple={false}
+              onChange={onUploadChange}
+              capture="environment"
+              disabled={isLoading}
+            ></input>
+          </div>
+          <div className="px-3">
+            <label className="btn btn-blue inline-flex" htmlFor="upload-img">
+              {isLoading ? (
+                <img
+                  src="/loading.svg"
+                  className="animate-spin h-5 w-5 mr-3"
+                ></img>
+              ) : null}
+              上传
+            </label>
+            <input
+              ref={uploadImgInputRef}
+              id="upload-img"
+              type="file"
+              accept="image/*"
+              className="file-input"
+              multiple={false}
+              onChange={onUploadChange}
+              disabled={isLoading}
+            ></input>
+          </div>
+          <img
+            ref={uploadImgRef}
+            src={uploadImg}
+            className="absolute invisible"
+          />
         </div>
       </div>
-      <ImageUpload
-          value={referImage}
-          onChange={(newValue?: string) => onUploadReferImage(newValue || '')}
-      />
       {isResultShow ? (
         <div className="flex h-full w-full flex-col items-center justify-center z-20 mask absolute">
           <img src={resultImg} className="max-w-80 max-h-80"></img>
           <div className="text-2xl pt-5 pb-10 break-all text-center px-8">
             {resultText}
           </div>
-          <button className="btn btn-blue inline-flex" onClick={() => reset()}>
+          <button className="btn btn-blue inline-flex" onClick={reset}>
             下一张
           </button>
         </div>
