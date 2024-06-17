6039f7399321246db6e0222e54d34dda724a8998
shichen <55756122+chenbaiyujason@users.noreply.github.com>
2024-06-17 16:32:37 +0800

    更新图片

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'tan': '#d2b48c', // 定义淡褐色
+      },
+      boxShadow: {
+        'tan': '0 4px 6px -1px rgba(210, 180, 140, 0.1), 0 2px 4px -1px rgba(210, 180, 140, 0.06)', // 自定义淡褐色阴影
+      }
     },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 };
 export default config;
